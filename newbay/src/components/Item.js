47b51db3ecb7c597a81c3500a8e1671742ea6d26
kaschief <kaschiefj@gmail.com>
2019-10-02 14:59:56 +0200

    working

--- newbay/src/components/Item.js
+++ newbay/src/components/Item.js
@@ -1,23 +1,92 @@
 import React from 'react';
-// import { StyleSheet, css } from 'aphrodite/no-important';
+import { StyleSheet, css } from 'aphrodite/no-important';
 
-// const styles = StyleSheet.create({
-//   buttonContainer: {
-//     display: 'flex',
-//     justifyContent: 'center'
-//   },
-//   button: {
-//     border: '1px solid lightGrey',
-//     borderRadius: '5px'
-//   },
-//   link: {
-//     color: 'black',
-//     textDecoration: 'none'
-//   }
-// });
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    background: 'rgb(255, 255, 255)',
+    width: '700px',
+    height: '80px',
+    borderRadius: '8px',
+    boxShadow:
+      'rgba(0, 0, 0, 0.05) 0px 0px 0px, rgba(0, 0, 0, 0.13) 0px 0px 0px;',
+    marginBottom: '16px',
+    overflow: 'hidden'
+  },
+  imageContainer: {
+    height: '80px',
+    minWidth: '80px',
+    maxWidth: '80px',
+    // paddingTop: '30px',
+    backgroundColor: 'blue'
+  },
+  contentContainer: {
+    height: '80px',
+    flex: '1',
+    backgroundColor: 'yellow',
+    display: 'flex',
+    flexDirection: 'column',
+    overflow: 'auto',
+    padding: '16px'
+  },
+  contents: {
+    marginBlockStart: '0',
+    marginBlockEnd: '0',
+    color: 'black'
+  },
+  header: {
+    fontSize: '14px',
+    fontWeight: '600',
+    paddingBottom: '4px',
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap'
+  },
+  text: {
+    fontSize: '14px'
+  },
+  link: {
+    textDecoration: 'none'
+  },
+  footer: {
+    marginTop: '16px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  shipping: {
+    color: 'green',
+    fontWeight: '600'
+  }
+});
 
-const ItemUI = () => {
-  return <div>Test Item</div>;
+const ItemUI = ({ title, price, link, destination, shippingCost }) => {
+  return (
+    <div className={css(styles.container)}>
+      <a className={css(styles.link)} href={link}>
+        <div className={css(styles.imageContainer)}></div>
+      </a>
+      <div className={css(styles.contentContainer)}>
+        <a className={css(styles.link)} href={link}>
+          <h4 className={css(styles.contents, styles.header)}>{title}</h4>
+        </a>
+        <div className={css(styles.footer, styles.text)}>
+          {shippingCost > 0 ? (
+            <p className={css(styles.contents, styles.text, styles.shipping)}>
+              Ships {destination} for ${shippingCost}
+            </p>
+          ) : (
+            <p className={css(styles.contents, styles.text, styles.shipping)}>
+              Free shipping {destination}
+            </p>
+          )}
+          <p className={css(styles.contents, styles.header)}>{`$${price}`}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ItemUI;
--- newbay/src/components/ItemsContainer.js
+++ newbay/src/components/ItemsContainer.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import { StyleSheet, css } from 'aphrodite/no-important';
-import { compose, withProps } from 'recompose';
+// import { compose, withProps, lifecycle } from 'recompose';
 import axios from 'axios';
-import { RSA_NO_PADDING } from 'constants';
-import { resolve } from 'path';
+import Item from './Item';
 
 // const styles = StyleSheet.create({
 //   buttonContainer: {
@@ -20,32 +19,60 @@ import { resolve } from 'path';
 //   }
 // });
 
-const ItemsContainerUI = () => {
-  return <div>Groups of Items</div>;
-};
+export default class ItemsContainerUI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
 
-const addItems = withProps(props => {
-  axios
-    .get(
-      'https://cors-anywhere.herokuapp.com/https://svcs.sandbox.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SECURITY-APPNAME=WandoInt-217b-42d8-a699-e79808dd505e&keywords=cars&RESPONSE-DATA-FORMAT=JSON&itemFilter(0).name=MaxPrice&itemFilter(0).value=10.0'
-    )
-    .then(
-      response => {
-        console.log(
-          'RESPONSE',
-          response.data.findItemsByKeywordsResponse[0].searchResult[0].item
+  componentDidMount(props) {
+    axios
+      .get(
+        'https://cors-anywhere.herokuapp.com/https://svcs.sandbox.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SECURITY-APPNAME=WandoInt-217b-42d8-a699-e79808dd505e&keywords=cars&RESPONSE-DATA-FORMAT=JSON&itemFilter(0).name=MaxPrice&itemFilter(0).value=10.0'
+      )
+      .then(
+        response => {
+          this.setState({
+            items:
+              response.data.findItemsByKeywordsResponse[0].searchResult[0].item
+          });
+          console.log('STATE', this.state.items);
+
+          console.log(
+            'SHIPPING INFO',
+            this.state.items[0].shippingInfo[0].shipToLocations[0]
+          );
+        },
+        error => {
+          console.log('ERROR', error);
+        }
+      );
+  }
+
+  render() {
+    return this.state.items.length === 0 ? (
+      <div>Loading</div>
+    ) : (
+      this.state.items.map((a, i) => {
+        return (
+          <Item
+            key={i}
+            link={a.viewItemURL[0]}
+            title={a.title}
+            price={Number(
+              Math.round(a.sellingStatus[0].currentPrice[0].__value__ * 100) /
+                100
+            ).toFixed(2)}
+            ships={a.location[0].slice(0, -7)}
+            destination={a.shippingInfo[0].shipToLocations[0]}
+            shippingCost={Number(
+              a.shippingInfo[0].shippingServiceCost[0].__value__
+            ).toFixed(2)}
+          />
         );
-      },
-      error => {
-        console.log('ERROR', error);
-      }
+      })
     );
-
-  return {
-    items: 'hello'
-  };
-});
-
-const ItemsContainer = compose(addItems)(ItemsContainerUI);
-
-export default ItemsContainer;
+  }
+}
